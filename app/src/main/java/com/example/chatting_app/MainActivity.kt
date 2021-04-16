ff4a627dcec4093f8a36584acee5c3fa6c5df377
yunsung <12345ggh81@gmail.com>
2021-04-16 19:00:46 +0900

    Chatting_App

--- app/src/main/java/com/example/chatting_app/MainActivity.kt
+++ app/src/main/java/com/example/chatting_app/MainActivity.kt
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toolbar
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.example.chatting_app.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayout
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
     private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,5 +27,22 @@ class MainActivity : AppCompatActivity() {
 
         val tableLayout : TabLayout = binding.tabLayout
         val viewPager : ViewPager = binding.ViewPager
+    }
+
+    //자바에서 제한하는 패키지 단위와는 달리 동일한 모듈 내에 있는 클래스들의 접근을 제한하는 접근제한자
+    //private와 비
+    internal class ViewPagerAdap슷ter(
+            val tabcount : Int,
+            fragmentManager: FragmentManager,
+            //프래그먼트 갯수가 정해졌을때 사용
+    ):FragmentPagerAdapter(fragmentManager){
+        override fun getCount(): Int {
+            // 페이지의 전체 크기를 리턴해준다.
+            return tabcount
+        }
+
+        override fun getItem(position: Int): Fragment {
+
+        }
     }
 }
